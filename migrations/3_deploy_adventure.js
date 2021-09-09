313470a13e8762c8217240c2ee9873dcc192ebff
VitalR <vitalii.rodikov@gmail.com>
2021-09-09 16:31:48 +0300

    update adventure deployment script

--- migrations/3_deploy_adventure.js
+++ migrations/3_deploy_adventure.js
@@ -1,15 +1,10 @@
 const AdventureTime = artifacts.require("adventure-time.sol")
-// const DaycareManager = artifacts.require("daycare-manager.sol")
 
 module.exports = async (deployer, network, accounts) => {
     await deployer.deploy(AdventureTime);
     const adventureTime = await AdventureTime.deployed();
     console.log(adventureTime.address);
     pausePromise('adventureTime');
-
-    // await deployer.deploy(DaycareManager);
-    // const daycareManager = await DaycareManager.deployed();
-    // pausePromise('daycareManager');
 }
 
 function pausePromise(message, durationInSeconds = 1) {
